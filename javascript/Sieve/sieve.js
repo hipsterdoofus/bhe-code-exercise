d79accf676e2ee91874cafe8829afbb52dd7a9d3
Aurelius Capellan <aurelius@gmail.com>
2024-08-03 13:19:14 -0700

    Working implementation of Sieve class and method. Takes a long time to run tests.

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,7 +1,31 @@
 class Sieve {
+  constructor() {
+    this.primes = populatePrimes();
+  }
+
   NthPrime(n) {
-    throw new Error("unimplemented");
+    return this.primes[n];
   }
 }
+
+populatePrimes = (n = 1000000000) => {
+  const max = n;
+  const nums = new Uint8Array(max).fill(true);
+
+  for(let i = 0; i <= Math.sqrt(max); i++) {
+    if (nums[i]) {
+      const num = i + 2; // number not index
+      for(let j = num * num; j < max; j += num) {
+        const multiple = j - 2;
+        nums[multiple] = false;
+      }
+    }
+  }
+  
+  return nums.reduce((acc, isPrime, index) => {
+      if (isPrime) acc.push(index + 2);
+      return acc;
+  }, []);
+};
 
 module.exports = Sieve;
